9e692bfa120a9f6b4b76765381a2d0bafe2c8cbd
shubham6369 <shubham67257@gmail.com>
2026-09-21 12:57:13 +0530

    Linkedlist questions on leetcode

--- Linkedlistindsa/LinkedListExample.java
+++ Linkedlistindsa/LinkedListExample.java
@@ -7,6 +7,7 @@ public class LinkedListExample {
       list.add(30);
       list.add(40);
       list.add(50);
+      list.add(45, 5);
         
         list.print();
    }
@@ -28,24 +29,50 @@ class LinkedList {
       Node newNode = new Node(data);
 
       if (head == null) {
-          head = newNode;
-          return;
+         head = newNode;
+         return;
       }
 
       Node curr = head;
-             while(curr.next != null){
+             while(curr.next != null);{
                curr = curr.next;
       }
 
       curr.next = newNode;
    }
    
+   void add(int data, int n) {
+      Node newNode = new Node(data);
+
+      if(n == 1){
+         newNode.next = head;
+         head = newNode;
+         return;
+      }
+
+      for (int i = 1; i < n - 1; i++ ) {
+         curr = curr.next;
+      }
+      if (curr != null);{
+         return; 
+      }
+          newNode.next = curr.next;
+         curr.next = newNode;
+      }else{
+         System.out.println("Invalid position");
+      }   
+   }  
+
    void print(){
       Node curr = head;
       while (curr.next != null) {
          System.out.print(curr.data + " -> ");
          curr = curr.next;
       }
-      System.out.println("null");   
+      System.out.println("null"); 
+      
+      
+      }
+ 
    }
-}   
+}
